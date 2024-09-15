05f1fa341e1c2347d1c8d7c043590def961acefb
draven-mcconathy <draveniii@gmail.com>
2024-09-15 17:40:10 -0400

    I did it!

---
+++ Knowledge_Check_2/Knowledge_Check_2/Bulbasaur.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Check_2
+{
+    internal class Bulbasaur : Pokemon
+    {
+        public string Ability {  get; set; }
+        public string Nature { get; set; }
+
+        public Bulbasaur()
+        {
+            NickName = "";
+            Ability = "";
+            Nature = "";
+        }
+
+        new public string ToString()
+        {
+            return $"\nName: {NickName}\nAbility: {Ability}\nNature: {Nature}\n";
+        }
+    }
+}
---
+++ Knowledge_Check_2/Knowledge_Check_2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Knowledge_Check_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+            var recordList = new List<Bulbasaur>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var myClass = new Bulbasaur();
+
+                Console.WriteLine("Enter the value for Bulbasaurs nickname: ");
+                myClass.NickName = Console.ReadLine();
+
+                Console.WriteLine("Enter the value for his ability: ");
+                myClass.Ability = Console.ReadLine();
+
+                Console.WriteLine("Enter the value for his nature: ");
+                myClass.Nature = Console.ReadLine();
+
+                recordList.Add(myClass);
+            }
+
+            // Print out the list of records using Console.WriteLine()
+            Console.WriteLine("Bulbasaurs:");
+
+            foreach (Bulbasaur pokemon in recordList)
+            {
+                Console.WriteLine(pokemon.ToString());
+            }
+        }
+    }
+}
